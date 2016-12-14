60a8c3de28a4ae4da0da024cca478a281bd2763e
Archer <421228286@qq.com>
2016-12-14 11:34:43 +0800

    完成自定义开关绘制内容

---
+++ app/src/main/java/com/example/archer/selfdefiningwidget/activity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.archer.selfdefiningwidget.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.archer.selfdefiningwidget.R;
+import com.example.archer.selfdefiningwidget.ui.ToggleView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleView toggleView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //1.拿到自定义好的toggleView实例
+        toggleView = (ToggleView) findViewById(R.id.ToggleView);
+        //2.创建需要的方法
+        toggleView.setSwitchBackgroundResource(R.drawable.switch_background);
+
+        toggleView.setSlideButtonResource(R.drawable.slide_button);
+
+
+
+        toggleView.setSwitchStatus(true);//设置开关的默认值。
+    }
+}
---
+++ app/src/main/java/com/example/archer/selfdefiningwidget/ui/ToggleView.java
@@ -0,0 +1,163 @@
+package com.example.archer.selfdefiningwidget.ui;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 完全自定义一个开关的控件
+ * Created by Archer on 2016/12/14.
+ *
+ * 得到三个构造函数，根据传入的参数选择对应的构造函数
+ *
+ *
+ *Android界面的绘制：
+ *
+ *
+ * 1.  measure(测量) -> layout（摆放）-> draw（绘制）。
+ *
+ * 2. onMeasure()->onLayout()->onDraw()        重写这些方法，实现自定义控件
+ *
+ * onResume() 方法后执行
+ *
+ *View：
+ *
+ *继承 View 没有onLayout方法 ： onMeasure(通过这个方法指定宽高)->onDraw()
+ *
+ *
+ *ViewGroup：
+ *这个可以理解为当你要摆放一组view的时候，就需要layout方法来确定哪一个view摆放在哪。
+ * onMeasure(通过这个方法指定宽高)->onLayout(摆放views)->onDraw()
+ *
+ *
+ *
+ */
+
+
+public class ToggleView  extends View{
+
+    private Bitmap switchBackground;
+    private Bitmap slideButton;
+    private Paint paint;
+
+    private boolean mSwitchStatus=false;
+
+    /**
+     *用于代码创建控件
+     *
+     * @param context 上下文
+     */
+    public ToggleView(Context context) {
+        super(context);
+        initPaint();
+
+    }
+
+    /**
+     *用于在xml种使用，可指定自定义属性。
+     * @param context 上下文
+     * @param attrs  属性
+     */
+    public ToggleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initPaint();
+
+    }
+
+    /**
+     * 用于代码创建控件
+     * *用于在xml种使用，可指定自定义属性。
+     * 指定样式
+     * @param context  上下文
+     * @param attrs 属性
+     * @param defStyleAttr  样式
+     */
+    public ToggleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initPaint();
+    }
+
+    private void initPaint() {
+        paint = new Paint();
+
+
+    }
+
+
+    /**
+     *
+     * @param switch_background  设置背景图片
+     */
+
+    public void setSwitchBackgroundResource(int switch_background) {
+
+        //使用bitmapFactory创建一个图片对象
+        switchBackground = BitmapFactory.decodeResource(getResources(), switch_background);
+
+
+    }
+
+    /**
+     *
+     * @param slide_button 设置背景滑动图片资源
+     */
+    public void setSlideButtonResource(int slide_button) {
+
+        slideButton = BitmapFactory.decodeResource(getResources(), slide_button);
+
+    }
+
+    /**
+     *
+     * @param mSwitchStatus 开关状态
+     */
+
+    public void setSwitchStatus(boolean mSwitchStatus) {
+
+        this.mSwitchStatus=mSwitchStatus;
+
+
+    }
+
+
+    /**
+     *
+     * 1.通过BitmapFactory拿到相应的对象，接着开始绘制图像
+     *  onMeasure(通过这个方法指定宽高)->onDraw()
+     */
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        //得到默认的图片的宽高
+        setMeasuredDimension(switchBackground.getWidth(),switchBackground.getHeight());
+
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //1.绘制背景
+        canvas.drawBitmap(switchBackground,0,0,paint);
+
+
+        if (mSwitchStatus){
+            //2.绘制slideButton
+            int newLeft = switchBackground.getWidth() - slideButton.getWidth();
+
+            canvas.drawBitmap(slideButton,newLeft,0,paint);
+
+        }else {
+            canvas.drawBitmap(slideButton,0,0,paint);
+
+
+        }
+
+
+
+    }
+}
